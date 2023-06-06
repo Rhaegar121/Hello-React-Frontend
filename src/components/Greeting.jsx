073b9167bfda047579dfba1b3e965e8c5064d24c
Rhaegar121 <kaungmyatkyaw7012@gmail.com>
2023-06-06 23:04:40 +0630

    create greeting component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchMessage } from './redux/messagesSlice';
+
+const Greeting = () => {
+  const messages = useSelector((state) => state.messages);
+
+  const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchMessage());
+    }, []);
+
+  if (messages.isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+        <>
+            <h1>
+                <span>Random Greeting: </span>
+                <span>{messages.message}</span>
+            </h1>
+        </>
+  );
+};
+
+export default Greeting;
